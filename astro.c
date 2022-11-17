b3a973710f98259d6d9c542e91c5d8b30465ebb0
Vincent Louis <vincent.louis.pro@gmail.com>
2022-11-17 15:37:27 +0100

    Delete astro.c

--- astro.c
+++
@@ -1,8 +0,0 @@
-#include "astro.h"
-#include <math.h>
-double getSchwarzschild(double masse, double vitesse, int param)
-{
-    if(masse > 0 || vitesse !=0 || param !=0){
-        return (double) (2 * CONSTANTE_G * masse / (pow( vitesse, 2)));
-    }
-}
